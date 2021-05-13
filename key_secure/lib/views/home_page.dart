165d64993ded5bec2a62f01f2c08d48b97ab19f7
alokhegde2 <hegdealok@yahoo.com>
2021-05-13 14:27:03 +0530

    Added settings page

--- key_secure/lib/views/home_page.dart
+++ key_secure/lib/views/home_page.dart
@@ -6,189 +6,82 @@ import 'package:key_secure/controllers/password_controller.dart';
 import 'package:key_secure/views/add_password.dart';
 import 'package:key_secure/views/all_items.dart';
 import 'package:key_secure/views/auth/login/master_pass.dart';
-import 'package:key_secure/views/auth/main_auth.dart';
+import 'package:key_secure/views/settings.dart';
 import 'package:key_secure/widgets/search_bar.dart';
 
-import '../main.dart';
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _selectedDestination = 0;
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final passwordController = Get.put(PasswordController());
     return Scaffold(
-        appBar: AppBar(
-          title: InkWell(
-            onTap: () {
-              Get.to(MasterPass());
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(CupertinoIcons.gear_alt),
+          onPressed: () {
+            Get.to(
+              Settings(),
+            );
+          },
+        ),
+        title: InkWell(
+          onTap: () {
+            Get.to(MasterPass());
+          },
+          child: Text(
+            "Key Secure",
+            style: GoogleFonts.ubuntu(color: Colors.white),
+          ),
+        ),
+        // centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(CupertinoIcons.search),
+            onPressed: () {
+              showSearch(context: context, delegate: SearchBar());
             },
-            child: Text(
-              "Key Secure",
-              style: GoogleFonts.ubuntu(color: Colors.white),
-            ),
           ),
-          // centerTitle: true,
-          actions: [
-            // IconButton(
-            //   icon: Icon(CupertinoIcons.refresh),
-            //   onPressed: () {
-            //     passwordController.onInit();
-            //   },
-            // ),
-            IconButton(
-              icon: Icon(CupertinoIcons.search),
+          IconButton(
+              icon: Icon(CupertinoIcons.add),
               onPressed: () {
-                showSearch(context: context, delegate: SearchBar());
-              },
-            ),
-            IconButton(
-                icon: Icon(CupertinoIcons.add),
-                onPressed: () {
-                  Get.to(AddPassword());
-                }),
-            PopupMenuButton(
-              child: Center(child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Icon(CupertinoIcons.ellipsis_vertical),
-              )),
-              itemBuilder: (context) => [
-                PopupMenuItem(
-                  value: 1,
-                  child: Text("Refresh"),
-                ),
-                PopupMenuItem(
-                  value: 2,
-                  child: Text("Settings"),
-                ),
-              ],
-              onSelected: (value) {
-                switch (value) {
-                  case 1:
-                    passwordController.onInit();
-                    break;
-                  case 2:
-                    print("Value 2");
-                    break;
-                  default:
-                }
-              },
-            ),
-          ],
-        ),
-        drawer: SafeArea(
-          child: Drawer(
-            child: Container(
-              child: ListView(
-                // Important: Remove any padding from the ListView.
-                padding: EdgeInsets.zero,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      'Collections',
-                      style: GoogleFonts.poppins(),
-                    ),
-                  ),
-                  Divider(
-                    height: 1,
-                    thickness: 1,
-                  ),
-                  ListTile(
-                    leading: Icon(CupertinoIcons.shield_lefthalf_fill),
-                    title: Text('All Item'),
-                    selected: _selectedDestination == 0,
-                    onTap: () => selectDestination(0),
-                  ),
-                  ListTile(
-                    leading: Icon(CupertinoIcons.star),
-                    title: Text('Favourite'),
-                    selected: _selectedDestination == 1,
-                    onTap: () => selectDestination(1),
-                  ),
-                  Divider(
-                    height: 1,
-                    thickness: 1,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      'My Account',
-                      style: GoogleFonts.poppins(),
-                    ),
-                  ),
-                  ListTile(
-                    leading: Icon(CupertinoIcons.person),
-                    title: Text('Account Details'),
-                    selected: _selectedDestination == 2,
-                    onTap: () => selectDestination(2),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.security_sharp),
-                    title: Text('Change Master Pin'),
-                    selected: _selectedDestination == 3,
-                    onTap: () => selectDestination(3),
-                  ),
-                  Divider(
-                    height: 1,
-                    thickness: 1,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      'Key Secure',
-                      style: GoogleFonts.poppins(),
-                    ),
-                  ),
-                  ListTile(
-                    leading: Icon(CupertinoIcons.person),
-                    title: Text('About Us'),
-                    selected: _selectedDestination == 4,
-                    onTap: () => selectDestination(4),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.help),
-                    title: Text('Support Us'),
-                    selected: _selectedDestination == 5,
-                    onTap: () => selectDestination(5),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.share_outlined),
-                    title: Text('Share'),
-                    selected: _selectedDestination == 6,
-                    onTap: () => selectDestination(6),
-                  ),
-                  ListTile(
-                      leading: Icon(Icons.exit_to_app_outlined),
-                      title: Text('Log out'),
-                      selected: _selectedDestination == 7,
-                      onTap: () {
-                        selectDestination(7);
-                        box.erase();
-                        Get.off(MainAuth());
-                      }),
-                ],
+                Get.to(AddPassword());
+              }),
+          PopupMenuButton(
+            child: Center(
+                child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(CupertinoIcons.ellipsis_vertical),
+            )),
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                value: 1,
+                child: Text("Refresh"),
               ),
-            ),
+              PopupMenuItem(
+                value: 2,
+                child: Text("Settings"),
+              ),
+            ],
+            onSelected: (value) {
+              switch (value) {
+                case 1:
+                  passwordController.onInit();
+                  break;
+                case 2:
+                  print("Value 2");
+                  break;
+                default:
+              }
+            },
           ),
-        ),
-        body: Obx(
-          () => (passwordController.passwordList.length != 0)
-              ? AllItems()
-              : Center(
-                  child: Text("Nothing Found"),
-                ),
-        ));
-  }
-
-  void selectDestination(int index) {
-    setState(() {
-      _selectedDestination = index;
-    });
+        ],
+      ),
+      body: Obx(
+        () => (passwordController.passwordList.length != 0)
+            ? AllItems()
+            : Center(
+                child: Text("Nothing Found"),
+              ),
+      ),
+    );
   }
 }
---
+++ key_secure/lib/views/settings.dart
@@ -0,0 +1,273 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:key_secure/controllers/user_controller.dart';
+import 'package:key_secure/views/static/privacy_policy.dart';
+
+import '../main.dart';
+import 'auth/main_auth.dart';
+
+class Settings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final userController = Get.put(UserController());
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(CupertinoIcons.back),
+          onPressed: () {
+            Get.back();
+          },
+        ),
+        title: Text(
+          "Settings",
+          style: GoogleFonts.ubuntu(color: Colors.white),
+        ),
+      ),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Obx(
+            () => ListView(
+              children: [
+                (userController.userList.length != 0)
+                    ? Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            userController.userList[0].name,
+                            style: GoogleFonts.poppins(
+                                fontWeight: FontWeight.w600, fontSize: 25.0),
+                          ),
+                          Text(
+                            userController.userList[0].email,
+                            style: GoogleFonts.poppins(
+                                fontWeight: FontWeight.w300, fontSize: 15.0),
+                          ),
+                        ],
+                      )
+                    : Center(child: CircularProgressIndicator()),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Divider(
+                  height: 8,
+                  color: Colors.grey.shade400,
+                ),
+                SizedBox(
+                  height: 25.0,
+                ),
+                Text(
+                  "My Account",
+                  style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w600, fontSize: 20.0),
+                ),
+                SizedBox(
+                  height: 25.0,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Row(
+                    children: [
+                      Icon(CupertinoIcons.lock),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        "Change Password",
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w500, fontSize: 17.0),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Divider(
+                  height: 8,
+                  color: Colors.grey.shade400,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Row(
+                    children: [
+                      Icon(Icons.security),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        "Change Master Password",
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w500, fontSize: 17.0),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 45.0,
+                ),
+                Text(
+                  "Key Secure",
+                  style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w600, fontSize: 20.0),
+                ),
+                SizedBox(
+                  height: 25.0,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Row(
+                    children: [
+                      Icon(CupertinoIcons.info_circle),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        "About us",
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w500, fontSize: 17.0),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Divider(
+                  height: 8,
+                  color: Colors.grey.shade400,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Row(
+                    children: [
+                      Icon(CupertinoIcons.person_3),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        "Support Us",
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w500, fontSize: 17.0),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Divider(
+                  height: 8,
+                  color: Colors.grey.shade400,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Row(
+                    children: [
+                      Icon(CupertinoIcons.share),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        "Share",
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w500, fontSize: 17.0),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Divider(
+                  height: 8,
+                  color: Colors.grey.shade400,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                InkWell(
+                  onTap: () {
+                    Get.to(PrivacyPolicy());
+                  },
+                  child: Row(
+                    children: [
+                      Icon(CupertinoIcons.exclamationmark_shield),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        "Privacy Policy",
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w500, fontSize: 17.0),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Divider(
+                  height: 8,
+                  color: Colors.grey.shade400,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                InkWell(
+                  onTap: () async {
+                    box.erase();
+                    Get.off(MainAuth());
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Logout",
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w500, fontSize: 17.0),
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Icon(CupertinoIcons.power),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Divider(
+                  height: 8,
+                  color: Colors.grey.shade400,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  "App Version v 0.1",
+                  style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w500, fontSize: 17.0),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
