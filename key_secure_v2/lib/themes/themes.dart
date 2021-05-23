507c5e0280eadf260fd5e2122a7b43826cabce6b
alokhegde2 <hegdealok@yahoo.com>
2021-05-23 17:17:15 +0530

    Added appbar theme

--- key_secure_v2/lib/themes/themes.dart
+++ key_secure_v2/lib/themes/themes.dart
@@ -4,10 +4,20 @@ class Themes {
   final lightTheme = ThemeData.light().copyWith(
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.white,
-    brightness: Brightness.light
+    brightness: Brightness.light,
+    appBarTheme: AppBarTheme(
+      color: Colors.white,
+      iconTheme: IconThemeData(color: Colors.black),
+      elevation: 0,
+    ),
   );
   final darkTheme = ThemeData.dark().copyWith(
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.grey[900],
+    appBarTheme: AppBarTheme(
+      color: Colors.grey[900],
+      iconTheme: IconThemeData(color: Colors.white),
+      elevation: 0,
+    ),
   );
 }
