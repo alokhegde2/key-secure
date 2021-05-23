2577f132ea973628c4b4f0c7f946615be9dec993
alokhegde2 <hegdealok@yahoo.com>
2021-05-23 18:39:02 +0530

    Added appbar title color

--- key_secure_v2/lib/themes/themes.dart
+++ key_secure_v2/lib/themes/themes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Themes {
   final lightTheme = ThemeData.light().copyWith(
@@ -8,6 +9,11 @@ class Themes {
     appBarTheme: AppBarTheme(
       color: Colors.white,
       iconTheme: IconThemeData(color: Colors.black),
+      textTheme: TextTheme(
+        headline6: GoogleFonts.poppins(
+            color: Colors.black, fontSize: 20, fontWeight: FontWeight.w600),
+      ),
+      titleTextStyle: GoogleFonts.poppins(color: Colors.black),
       elevation: 0,
     ),
   );
