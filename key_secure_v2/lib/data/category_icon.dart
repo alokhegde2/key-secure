d51bd4ce645eede92fe3aa6abc7448bcf5663482
alokhegde2 <hegdealok@yahoo.com>
2021-05-24 15:40:35 +0530

    Added the category icons data

---
+++ key_secure_v2/lib/data/category_icon.dart
@@ -0,0 +1,12 @@
+final Map categoryIcon = {
+  "social":"https://image.flaticon.com/icons/png/512/2076/2076218.png",
+  "finance":"https://image.flaticon.com/icons/png/512/631/631180.png",
+  "entertainment":"https://image.flaticon.com/icons/png/512/860/860331.png",
+  "food_and_drink":"https://image.flaticon.com/icons/png/512/851/851554.png",
+  "education":"https://image.flaticon.com/icons/png/512/2939/2939207.png",
+  "dating":"https://image.flaticon.com/icons/png/512/2545/2545260.png",
+  "design":"https://image.flaticon.com/icons/png/512/1496/1496034.png",
+  "business":"https://image.flaticon.com/icons/png/512/784/784712.png",
+  "games":"https://image.flaticon.com/icons/png/512/686/686589.png",
+  "shopping":"https://image.flaticon.com/icons/png/512/3091/3091221.png"
+};
