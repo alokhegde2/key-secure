bbe5e1debf46810e0f54249887c60d3bf695254f
alokhegde2 <hegdealok@yahoo.com>
2021-06-11 23:02:30 +0530

    Added login screen✌🏽

---
+++ key_secure_v2/lib/views/auth_screens/loginpage.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:key_secure_v2/constants.dart';
+import 'package:key_secure_v2/views/homepage.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({Key? key}) : super(key: key);
+  //email controller get email of user
+  final _emailController = TextEditingController();
+  //password controller to get password from user
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 18.0),
+              child: Image.asset(
+                "assets/logo/logo.png",
+                height: 80,
+                width: 80.0,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Center(
+                child: Text(
+                  "Key Secure",
+                  style: GoogleFonts.poppins(
+                    fontSize: 28,
+                    fontWeight: FontWeight.w600,
+                    color: kMainColor,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Center(
+              child: Text(
+                "Sign In with your email and password",
+                style: GoogleFonts.poppins(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            TextFormField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  labelText: "Email",
+                  hintText: "johndoe@gmail.com",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                  ),
+                  focusColor: Colors.white,
+                  prefixIcon: Icon(CupertinoIcons.mail)),
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            TextFormField(
+              controller: _passwordController,
+              keyboardType: TextInputType.visiblePassword,
+              decoration: InputDecoration(
+                  labelText: "Password",
+                  hintText: "*************",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                  ),
+                  focusColor: Colors.white,
+                  prefixIcon: Icon(CupertinoIcons.lock),
+                  suffixIcon: IconButton(
+                      onPressed: () {}, icon: Icon(CupertinoIcons.eye))),
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            InkWell(
+              onTap: () {
+                // Get.to(HomePage());
+                Get.toNamed('/home');
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: kMainColor,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Center(
+                  child: Text(
+                    "Sign In",
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+                height: 50.0,
+              ),
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                "Forgot Password",
+                style: GoogleFonts.poppins(
+                    color: Colors.grey,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w700,
+                    decoration: TextDecoration.underline),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Dont't have an account?",
+                  style: GoogleFonts.poppins(),
+                ),
+                TextButton(
+                  onPressed: () {
+                    // Get.to(OnBoard());
+                  },
+                  child: Text(
+                    "Sign Up",
+                    style: GoogleFonts.poppins(
+                      color: kMainColor,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
