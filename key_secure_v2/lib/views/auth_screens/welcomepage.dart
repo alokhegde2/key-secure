77528ada3e3dbeb713e582d0387481e21f49f812
alokhegde2 <hegdealok@yahoo.com>
2021-06-13 12:56:15 +0530

    Added welcome screen

---
+++ key_secure_v2/lib/views/auth_screens/welcomepage.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:key_secure_v2/constants.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.red,
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(
+                  left: 20.0, right: 20.0, bottom: 20.0, top: 80),
+              height: MediaQuery.of(context).size.height * 0.90,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://image.freepik.com/free-vector/creativity-concept-illustration_114360-1083.jpg",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(50.0),
+                      bottomLeft: Radius.circular(50.0))),
+              child: null,
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  left: 20.0, right: 20.0, top: 15.0, bottom: 5.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Get.toNamed('/register');
+                    },
+                    child: Text(
+                      "REGISTER",
+                      style: GoogleFonts.poppins(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  VerticalDivider(
+                    thickness: 2.0,
+                    color: Colors.grey,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Get.toNamed('/login');
+                    },
+                    child: Text(
+                      "LOGIN",
+                      style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
