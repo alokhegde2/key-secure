df206e1c531d5b7c221c8d846796fee30dda15c7
alokhegde2 <hegdealok@yahoo.com>
2021-06-13 12:52:02 +0530

    Added some routes and page transition

--- key_secure_v2/lib/main.dart
+++ key_secure_v2/lib/main.dart
@@ -8,10 +8,13 @@ import 'package:key_secure_v2/themes/themes.dart';
 import 'package:key_secure_v2/views/auth_screens/forgot_passwordpage.dart';
 import 'package:key_secure_v2/views/auth_screens/loginpage.dart';
 import 'package:key_secure_v2/views/auth_screens/mail_sentpage.dart';
+import 'package:key_secure_v2/views/auth_screens/masterpasspage.dart';
 import 'package:key_secure_v2/views/auth_screens/registerpage.dart';
+import 'package:key_secure_v2/views/auth_screens/welcomepage.dart';
 import 'package:key_secure_v2/views/homepage.dart';
 import 'package:key_secure_v2/views/searchpage.dart';
 import 'package:key_secure_v2/views/settings.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() async {
   await GetStorage.init();
@@ -29,11 +32,13 @@ class MyApp extends StatelessWidget {
       darkTheme: Themes().darkTheme,
       themeMode: ThemeServices().getThemeMode(),
       home: AnimatedSplashScreen(
+        duration: 3000,
         splash: "assets/logo/logo.png",
         backgroundColor: kMainColor,
-        nextScreen: LoginPage(),
-        animationDuration: Duration(seconds: 4),
+        nextScreen: WelcomePage(),
+        animationDuration: Duration(seconds: 3),
         splashTransition: SplashTransition.fadeTransition,
+        pageTransitionType: PageTransitionType.rightToLeft,
       ),
       // unknownRoute: GetPage(name: '/notfound', page: () => UnknownRoutePage()),
       // initialRoute: '/',
@@ -58,7 +63,7 @@ class MyApp extends StatelessWidget {
           page: () => LoginPage(),
           transition: Transition.rightToLeft,
         ),
-         GetPage(
+        GetPage(
           name: '/register',
           page: () => RegisterPage(),
           transition: Transition.rightToLeft,
@@ -71,6 +76,16 @@ class MyApp extends StatelessWidget {
         GetPage(
           name: '/mail-sent',
           page: () => MailSent(),
+          transition: Transition.rightToLeft,
+        ),
+        GetPage(
+          name: '/welcome',
+          page: () => WelcomePage(),
+          transition: Transition.rightToLeft,
+        ),
+        GetPage(
+          name: '/master-pass',
+          page: () => MasterPass(),
           transition: Transition.rightToLeft,
         ),
       ],
