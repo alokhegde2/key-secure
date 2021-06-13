c3d41cd67f9bca89dc3e584bdc6a7d3caac53e28
alokhegde2 <hegdealok@yahoo.com>
2021-06-13 12:51:19 +0530

    Added secondary screen

--- key_secure_v2/lib/constants.dart
+++ key_secure_v2/lib/constants.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 const kMainColor =Colors.blueAccent;
 const kSocialMediaColor = Colors.pinkAccent;
+const kSecondaryColor = Colors.red;
 const kwebchildAspectRatio = 2.1;
 const ktabchildAspectRatio = 1.2;
 const kmobilechildAspectRatio =0.65;
