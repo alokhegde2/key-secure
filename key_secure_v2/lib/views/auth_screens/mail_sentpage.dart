b2e3b3ab2f43e6aa14dab7237da78e11906d06ae
alokhegde2 <hegdealok@yahoo.com>
2021-06-13 12:54:05 +0530

    Made some changes

--- key_secure_v2/lib/views/auth_screens/mail_sentpage.dart
+++ key_secure_v2/lib/views/auth_screens/mail_sentpage.dart
@@ -70,8 +70,9 @@ class MailSent extends StatelessWidget {
                     height: 50.0,
                     width: 200.0,
                     decoration: BoxDecoration(
-                        color: kMainColor,
-                        borderRadius: BorderRadius.circular(10.0)),
+                      color: kMainColor,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                     child: Center(
                       child: Text(
                         "Continue",
