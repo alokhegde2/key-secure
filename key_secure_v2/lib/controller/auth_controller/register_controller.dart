bb95cb3358f6e0e86a81563fd3a0a2e01dc53022
alokhegde2 <hegdealok@yahoo.com>
2021-06-14 23:52:32 +0530

    Romoved snack bar

--- key_secure_v2/lib/controller/auth_controller/register_controller.dart
+++ key_secure_v2/lib/controller/auth_controller/register_controller.dart
@@ -1,11 +1,10 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class RegisterController extends GetxController {
   var isPasswordVisible = false.obs;
   var isConfirmPasswordVisible = false.obs;
   var isError = false.obs;
+  var error = "".obs;
 
   //togglling password visible
   togglePassword() {
@@ -27,27 +26,10 @@ class RegisterController extends GetxController {
 
   setError(err) {
     isError(true);
-    Get.snackbar(
-      "Error",
-      err,
-      snackPosition: SnackPosition.BOTTOM,
-      backgroundColor: Colors.redAccent,
-      borderRadius: 50.0,
-      margin: EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
-      messageText: Text(
-        err,
-        style: GoogleFonts.poppins(
-          color: Colors.white,
-          fontWeight: FontWeight.w500,
-          fontSize: 17.0,
-          letterSpacing: .9,
-        ),
-        textAlign: TextAlign.center,
-      ),
-      titleText: Text(
-        "",
-        style: GoogleFonts.poppins(fontSize: 0.0),
-      ),
-    );
+    error(err);
+  }
+
+  success() {
+    isError(false);
   }
 }
