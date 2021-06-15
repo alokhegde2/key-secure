dafd8ab47263805dd8e66e0adb300465894766bc
alokhegde2 <hegdealok@yahoo.com>
2021-06-15 23:06:18 +0530

    Handled request and response from server

--- key_secure_v2/lib/services/auth_services/register_services.dart
+++ key_secure_v2/lib/services/auth_services/register_services.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:key_secure_v2/main.dart';
 
 class RegisterServices extends GetConnect {
   //Registering the user
@@ -10,5 +11,25 @@ class RegisterServices extends GetConnect {
       "authType": "normal"
     };
     return post("http://192.168.43.173:3000/api/v2/user/register", data);
+  }
+
+  //Checking if mail is verified
+  Future<Response> verifyMail() {
+    var mail = box.read("email");
+    return get(
+      "http://192.168.43.173:3000/api/v2/user/register/verify-mail/$mail",
+    );
+  }
+
+  //Creating new master password
+  Future<Response> createMaster(password) {
+    var data = {
+      "masterPassword": password,
+    };
+    var mail = box.read("email");
+    return put(
+      "http://192.168.43.173:3000/api/v2/user/register/new-master-pass/$mail",
+      data,
+    );
   }
 }
