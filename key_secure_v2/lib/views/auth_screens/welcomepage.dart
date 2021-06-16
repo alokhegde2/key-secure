94cb829ff44438f5817237c2a3ba109efa1a6339
alokhegde2 <hegdealok@yahoo.com>
2021-06-16 15:39:08 +0530

    Added willpopscope

--- key_secure_v2/lib/views/auth_screens/welcomepage.dart
+++ key_secure_v2/lib/views/auth_screens/welcomepage.dart
@@ -7,67 +7,96 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.red,
-        body: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.only(
-                  left: 20.0, right: 20.0, bottom: 20.0, top: 80),
-              height: MediaQuery.of(context).size.height * 0.90,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(
-                      "assets/images/welcome.jpg",
-                    ),
-                    fit: BoxFit.cover,
-                  ),
-                  borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular(50.0),
-                      bottomLeft: Radius.circular(50.0))),
-              child: null,
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                  left: 20.0, right: 20.0, top: 15.0, bottom: 5.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      Get.toNamed('/register');
-                    },
-                    child: Text(
-                      "REGISTER",
-                      style: GoogleFonts.poppins(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  VerticalDivider(
-                    thickness: 2.0,
-                    color: Colors.grey,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Get.toNamed('/login');
-                    },
-                    child: Text(
-                      "LOGIN",
-                      style: GoogleFonts.poppins(
-                        color: Colors.white,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                ],
+    return WillPopScope(
+      onWillPop: () async {
+        var _lastPressedAt; //last click time
+        if (_lastPressedAt == null ||
+            DateTime.now().difference(_lastPressedAt) > Duration(seconds: 1)) {
+          //Re-timed after two clicks of more than 1 second
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                "Press again to exit the program",
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
-            )
-          ],
-        ));
+              backgroundColor: Colors.black,
+            ),
+          );
+          _lastPressedAt = DateTime.now();
+          return false;
+        }
+        return true;
+      },
+      child: Scaffold(
+          backgroundColor: Colors.red,
+          body: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(
+                    left: 20.0, right: 20.0, bottom: 20.0, top: 80),
+                height: MediaQuery.of(context).size.height * 0.90,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                        "assets/images/welcome.jpg",
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(50.0),
+                        bottomLeft: Radius.circular(50.0))),
+                child: null,
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: 20.0, right: 20.0, top: 15.0, bottom: 5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        // Get.toNamed('/register');
+                        Get.toNamed('/success');
+                      },
+                      child: Text(
+                        "REGISTER",
+                        style: GoogleFonts.poppins(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    VerticalDivider(
+                      thickness: 2.0,
+                      color: Colors.grey,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Get.toNamed(
+                          '/login',
+                          arguments: {"preRoute": "/welcome"},
+                        );
+                      },
+                      child: Text(
+                        "LOGIN",
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )),
+    );
   }
 }
