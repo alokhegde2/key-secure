818dd5bdba03bf35a3dc059229c50712751c5314
alokhegde2 <hegdealok@yahoo.com>
2021-06-16 16:51:14 +0530

    Added controller for login user

--- key_secure_v2/lib/controller/auth_controller/login_controller.dart
+++ key_secure_v2/lib/controller/auth_controller/login_controller.dart
@@ -1,14 +1,29 @@
 import 'package:get/get.dart';
 
-class LoginController extends GetxController{
+class LoginController extends GetxController {
   var isPasswordVisibile = false.obs;
+  var isError = false.obs;
+  var error = "".obs;
+  var buttonText = "Sign In".obs;
 
-  togglePassword(){
-    if(isPasswordVisibile.value){
+  togglePassword() {
+    if (isPasswordVisibile.value) {
       isPasswordVisibile(false);
     } else {
       isPasswordVisibile(true);
     }
   }
-  
+
+  setError(err) {
+    isError(true);
+    error(err);
+  }
+
+  success() {
+    isError(false);
+  }
+
+  setButtonText(text) {
+    buttonText(text);
+  }
 }
