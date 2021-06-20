eb64df0ce6c74d878124c15ff7fe403d12a4804e
alokhegde2 <hegdealok@yahoo.com>
2021-06-20 23:14:56 +0530

    Added some more controllers

--- key_secure_v2/lib/controller/user_controller/update_user_controller.dart
+++ key_secure_v2/lib/controller/user_controller/update_user_controller.dart
@@ -1,12 +1,20 @@
-import 'dart:io';
-
 import 'package:get/get.dart';
 
 class UpdateUserController extends GetxController {
   var uploadedImage = "".obs;
+  var buttonEnabled = true.obs;
+  var isLoading = false.obs;
 
   uploadImage(image) {
     uploadedImage(image);
     print(uploadedImage.value);
+  }
+
+  toggleButton() {
+    (buttonEnabled.value) ? buttonEnabled(false) : buttonEnabled(true);
+  }
+
+  toggleLoading() {
+    (isLoading.value) ? isLoading(false) : isLoading(true);
   }
 }
