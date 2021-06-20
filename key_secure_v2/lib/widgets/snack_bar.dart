6707b826e9eed46b53fb15f58eebbcc4573082da
alokhegde2 <hegdealok@yahoo.com>
2021-06-20 23:18:36 +0530

    Separated snack bar from main files

---
+++ key_secure_v2/lib/widgets/snack_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+errorSnack(String message, BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        "$message",
+        style: GoogleFonts.poppins(
+          color: Colors.white,
+          fontSize: 15,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      backgroundColor: Colors.red,
+    ),
+  );
+}
+
+successSnack(String message, BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        "$message",
+        style: GoogleFonts.poppins(
+          color: Colors.black,
+          fontSize: 15,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      backgroundColor: Colors.greenAccent,
+    ),
+  );
+}
