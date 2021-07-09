99d96b92032c1b62c671d52908f251e9939d2919
Alok Hegde <hegdealok@yahoo.com>
2021-07-09 16:05:37 +0530

    Added animation package and fixed overflow issue in verify master pass screen

--- key_secure_v2/lib/views/auth_screens/verify_master_pass.dart
+++ key_secure_v2/lib/views/auth_screens/verify_master_pass.dart
@@ -22,135 +22,138 @@ class MasterPass extends StatelessWidget {
       child: Scaffold(
         body: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 20.0),
-                Text(
-                  "Master password",
-                  style: GoogleFonts.poppins(
-                    fontSize: 28,
-                    fontWeight: FontWeight.w700,
+            padding: const EdgeInsets.all(18.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 15.0),
+                  Text(
+                    "Master password",
+                    style: GoogleFonts.poppins(
+                      fontSize: 28,
+                      fontWeight: FontWeight.w700,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Text(
-                  "Enter the master password associated with your account to contine.",
-                  style: GoogleFonts.poppins(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                    color: Color(0xFF556274),
+                  SizedBox(
+                    height: 20.0,
                   ),
-                ),
-                SizedBox(
-                  height: 30.0,
-                ),
-                Text(
-                  "Master Password",
-                  style: GoogleFonts.poppins(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                    color: Color(0xFF556274),
+                  Text(
+                    "Enter the master password associated with your account to contine.",
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF556274),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Obx(
-                  () => TextFormField(
-                    controller: _masterPassController,
-                    obscureText: (verifyMasterPassController
-                            .isMasterPasswordVisible.value)
-                        ? false
-                        : true,
-                    keyboardType: TextInputType.visiblePassword,
-                    decoration: InputDecoration(
-                      labelText: "Master Password",
-                      hintText: "****************",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10.0),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  Text(
+                    "Master Password",
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF556274),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Obx(
+                    () => TextFormField(
+                      controller: _masterPassController,
+                      obscureText: (verifyMasterPassController
+                              .isMasterPasswordVisible.value)
+                          ? false
+                          : true,
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                        labelText: "Master Password",
+                        hintText: "****************",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                        ),
+                        focusColor: Colors.white,
+                        prefixIcon: Icon(Icons.vpn_key),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            verifyMasterPassController.toggleMasterPass();
+                          },
+                          icon: Icon((verifyMasterPassController
+                                  .isMasterPasswordVisible.value)
+                              ? CupertinoIcons.eye_slash
+                              : CupertinoIcons.eye),
                         ),
                       ),
-                      focusColor: Colors.white,
-                      prefixIcon: Icon(Icons.vpn_key),
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          verifyMasterPassController.toggleMasterPass();
-                        },
-                        icon: Icon((verifyMasterPassController
-                                .isMasterPasswordVisible.value)
-                            ? CupertinoIcons.eye_slash
-                            : CupertinoIcons.eye),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Obx(
+                    () => Container(
+                      child: (verifyMasterPassController.isError.value)
+                          ? Center(
+                              child: ErrorMessage(
+                                  error:
+                                      verifyMasterPassController.error.value),
+                            )
+                          : null,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Center(
+                    child: TextButton(
+                      onPressed: () {
+                        Get.toNamed('/forgot-master-pass');
+                      },
+                      child: Text(
+                        "Forgot Master Password",
+                        style: GoogleFonts.poppins(
+                            color: Colors.grey,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w700,
+                            decoration: TextDecoration.underline),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Obx(
-                  () => Container(
-                    child: (verifyMasterPassController.isError.value)
-                        ? Center(
-                            child: ErrorMessage(
-                                error: verifyMasterPassController.error.value),
-                          )
-                        : null,
+                  SizedBox(
+                    height: 20.0,
                   ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Center(
-                  child: TextButton(
-                    onPressed: () {
-                      Get.toNamed('/forgot-master-pass');
+                  InkWell(
+                    onTap: () {
+                      // Get.toNamed('/home');
+                      var masterPassword = _masterPassController.text;
+                      submitMasterPass(
+                          masterPassword, verifyMasterPassController);
                     },
-                    child: Text(
-                      "Forgot Master Password",
-                      style: GoogleFonts.poppins(
-                          color: Colors.grey,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w700,
-                          decoration: TextDecoration.underline),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                InkWell(
-                  onTap: () {
-                    // Get.toNamed('/home');
-                    var masterPassword = _masterPassController.text;
-                    submitMasterPass(
-                        masterPassword, verifyMasterPassController);
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: kMainColor,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: Center(
-                      child: Obx(
-                        () => Text(
-                          "${verifyMasterPassController.buttonText.value}",
-                          style: GoogleFonts.poppins(
-                            color: Colors.white,
-                            fontSize: 15,
-                            fontWeight: FontWeight.w700,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: kMainColor,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Center(
+                        child: Obx(
+                          () => Text(
+                            "${verifyMasterPassController.buttonText.value}",
+                            style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                            ),
                           ),
                         ),
                       ),
+                      height: 50.0,
                     ),
-                    height: 50.0,
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
