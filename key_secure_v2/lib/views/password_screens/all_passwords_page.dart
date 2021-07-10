6ab9326d3178583047315b3cbab92bde70197dc2
Alok Hegde <hegdealok@yahoo.com>
2021-07-10 12:29:15 +0530

    Added all password page

---
+++ key_secure_v2/lib/views/password_screens/all_passwords_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AllPasswords extends StatelessWidget {
+  const AllPasswords({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("All Passwords"),
+      ),
+    );
+  }
+}
