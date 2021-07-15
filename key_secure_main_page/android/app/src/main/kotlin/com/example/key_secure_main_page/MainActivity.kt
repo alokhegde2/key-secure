aa7b178aab3f94a5402b40ce55034f5bedf1e076
Alok Hegde <hegdealok@yahoo.com>
2021-07-15 17:51:24 +0530

    Added initial file structures

---
+++ key_secure_main_page/android/app/src/main/kotlin/com/example/key_secure_main_page/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.key_secure_main_page
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ key_secure_main_page/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:key_secure_main_page/views/homePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Poppins',
+      ),
+      home: HomePage(),
+    );
+  }
+}
